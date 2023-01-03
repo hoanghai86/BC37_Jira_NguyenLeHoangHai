c5a66267927d7d2fcf33d1c8be2aec41a8ec5345
hoanghai86 <hainguyenlehoang86.gmail.com>
2023-01-03 17:03:44 +0700

    ''

---
+++ src/templates/HomeTemplate/HomeTemplate.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Header from "../../components/Home/Header/Header";
+
+export const HomeTemplate = (props) => {
+  const { Component, ...restParam } = props;
+  return (
+    <Route
+      {...restParam}
+      render={(propsRoute) => {
+        return (
+          <>
+            <Header />
+            <Component {...propsRoute} />
+          </>
+        );
+      }}
+    />
+  );
+};
