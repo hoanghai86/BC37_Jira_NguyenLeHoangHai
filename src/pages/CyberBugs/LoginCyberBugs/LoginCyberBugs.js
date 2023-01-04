6adbb83e6653e68c9eb7a9f498b67d0104bbbf88
hoanghai86 <hainguyenlehoang86.gmail.com>
2023-01-04 12:03:11 +0700

    ''

--- src/pages/CyberBugs/LoginCyberBugs/LoginCyberBugs.js
+++ src/pages/CyberBugs/LoginCyberBugs/LoginCyberBugs.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { Button, Input } from "antd";
 import { UserOutlined, LockOutlined, FacebookOutlined, TwitterOutlined } from "@ant-design/icons";
+import {withFormik} from "formik";
+import * as Yup from "yup";
 
+function LoginCyberBugs(props) {
 
-export default function LoginCyberBugs(props) {
+  console.log(props)
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = props;
+
   return (
-    <form className="container" style={{ height: window.innerHeight }}>
+    <form onSubmit={handleSubmit} className="container" style={{ height: window.innerHeight }}>
       <div className="d-flex flex-col justify-center items-center" style={{ height: window.innerHeight }}>
-        <h3 className="text-center mb-3">Login CyberBugs</h3>
+        <h3 className="text-center mb-3">{props.displayName}</h3>
         <div className="d-flex flex-col">
-        <Input name="email" size="large" placeholder="email" prefix={<UserOutlined />} className="w-72"/>
+        <Input onChange={handleChange} name="email" size="large" placeholder="email" prefix={<UserOutlined />} className="w-72"/>
         </div>
+        <div className="text-danger">{errors.email}</div>
         <div className="d-flex flex-col mt-3">
-        <Input name="password" size="large" placeholder="password" prefix={<LockOutlined />} className="w-72"/>
+        <Input onChange={handleChange} type="password" name="password" size="large" placeholder="password" prefix={<LockOutlined />} className="w-72"/>
         </div>
-        <Button size="large" className="mt-5 w-72" style={{backgroundColor:"rgb(102,117,223)", color:"#fff"}}>Login</Button>
+        <div className="text-danger">{errors.password}</div>
+        <Button htmlType="submit" size="large" className="mt-5 w-72" style={{backgroundColor:"rgb(102,117,223)", color:"#fff"}}>Login</Button>
         <div className="d-flex mt-3">
             <Button type="primary" shape="circle" icon={<FacebookOutlined />} size="large" className="mr-2 flex items-center justify-center hover:opacity-50" style={{backgroundColor:"#0e2989"}}></Button>
             <Button type="primary" shape="circle" icon={<TwitterOutlined />} size="large" className="flex items-center justify-center"></Button>
@@ -24,3 +38,20 @@ export default function LoginCyberBugs(props) {
 
   );
 }
+
+const LoginCyberBugsWithFormik = withFormik({
+  mapPropsToValues: () => ({ email: '', password: '' }),
+
+  validationSchema: Yup.object().shape({
+    email:Yup.string().required('Email is required!').email('email is invalid!'),
+    password:Yup.string().min(6,'password must have min 6 characters').max(32,'password have max 32 characters')
+  }),
+
+  handleSubmit: (values, { setSubmitting }) => {
+    console.log(values);
+  },
+
+  displayName: 'Login CyberBugs',
+})(LoginCyberBugs);
+
+export default LoginCyberBugsWithFormik;
--- src/templates/HomeTemplate/UserLoginTemplate.js
+++ src/templates/HomeTemplate/UserLoginTemplate.js
@@ -17,7 +17,7 @@ export const UserLoginTemplate = (props) =>{
     return <Route {...restRoute} render ={(propsRoute)=>{
         return <>
         <Layout>
-            <Sider width={window.innerWidth/2} style={{height:window.innerHeight, backgroundImage:"url(https://picsum.photos/500)"}}></Sider>
+            <Sider width={window.innerWidth/2} style={{height:window.innerHeight, backgroundImage:"url(https://picsum.photos/2000)", backgroundSize:"100%"}}></Sider>
 
             <Content>
                 <Component />
