c07a893bbd3e1773949cb4af66793fe4118ab76a
hoanghai86 <hainguyenlehoang86@gmail.com>
2023-01-18 01:39:22 +0700

    ''

--- src/components/Notification/NotificationComponent.js
+++ src/components/Notification/NotificationComponent.js
@@ -4,11 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function NotificationComponent(propss) {
   const openNotificationWithIcon = (type) => {
-    notification[type]({
-      message: "Notification Title",
-      description:
-        "This is the content of the notification. This is the content of the notification. This is the content of the notification.",
-    });
+    
   };
   return <div></div>;
 }
